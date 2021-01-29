ac85c01b45dfc0c9c4396fdf64450908d8a77364
Abhishek Mundada <39642078+MundadaAbhishek@users.noreply.github.com>
2021-01-29 15:42:46 +0530

    Added math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+#Hello Everyone
+#Implementing Basic Math Functions
+#Again
+def Add:
+    return x+y
+
+def Substract:
+    if x<y:
+        return ERROR
+    return x-y
+
+def Multiply:
+    return x*y
+
+def Division:
+    return x/y
